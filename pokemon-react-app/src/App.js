dc469e5f844b9463d5e93b188795df6269d4eadc
benedicandra18 <74496610+benedicandra18@users.noreply.github.com>
2021-10-29 19:46:46 +0300

    create react app, React Router&Redux dependencies

---
+++ pokemon-react-app/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+function App() {
+  return (
+    <Router>
+    <div className="App">
+    </div>
+    </Router>
+  );
+}
+
+export default App;
